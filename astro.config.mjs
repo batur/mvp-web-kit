26f4b1abb99401f4b35cf8419cf2f315b05571cf
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-01-25 13:30:59 +0300

    chore: init astro with integrations
    - astro
    - react
    - image w/ sharp
    - mdx
    - partytown
    - prefetch
    - sitemap
    - tailwind
    - vercel
    - sass

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import node from "@astrojs/node";
+import image from "@astrojs/image";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    react(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    mdx(),
+    partytown(),
+    prefetch(),
+    sitemap(),
+    tailwind(),
+  ],
+  output: "server",
+  adapter: vercel(),
+});
