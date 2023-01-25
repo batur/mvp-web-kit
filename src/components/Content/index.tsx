075552fc3f5f81867f8111cd26d265beac2f7e6d
Batur Akkurt <halidbaturakkurt@gmail.com>
2023-01-25 23:08:44 +0300

    feat: add context and state management

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,25 @@
+import type { FC } from "react";
+
+import { useStore } from "@nanostores/react";
+
+import { hello } from "@/contexts";
+
+const Content: FC = () => {
+  const helloAtom = useStore(hello.atom);
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    hello.atom.set(e.target.value);
+  }
+
+  return (
+    <div>
+      <h1>Content</h1>
+      <h2>Store</h2>
+      <p>{helloAtom}</p>
+      <p>{helloAtom.length}</p>
+      <input onChange={(e) => handleChange(e)} value={helloAtom} />
+    </div>
+  );
+};
+
+export default Content;
---
+++ src/contexts/hello.ts
@@ -0,0 +1,5 @@
+import { atom as nanoAtom } from "nanostores";
+
+const atom = nanoAtom<string>("Hello World");
+
+export default { atom };
